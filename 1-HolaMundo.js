5e6d6d81e2621106e57e979189cf226d876008d6
elbob-mx <rob7th@gmail.com>
2022-04-12 17:51:40 -0500

    js-test-03

---
+++ 1-HolaMundo.js
@@ -0,0 +1,9 @@
+console.log('Mi primer programa' + ' ' + 2);
+console.log(2 + 2);
+
+nombreMadre = "Dora";
+edadMadre = 61;
+
+console.log(nombreMadre);
+console.log("El nombre de mi madre es " + nombreMadre);
+console.log("La edad de mi madre es " + edadMadre);
---
+++ 1-Variables.js
@@ -0,0 +1,21 @@
+// nombres ------------------<>
+const nombreMaría = "María";
+const nombreTeresa = "Teresa"
+const nombrePadre = "José";
+const nombreHijo = "Jesús";
+
+console.log(nombreMaría);
+console.log(nombreTeresa)
+
+const nombreMadreSoltera = "María Pérez";
+const nombrePaís = "Venezuela";
+
+console.log(nombreMadreSoltera);
+console.log(nombreMaría);
+console.log(nombrePaís);
+
+// edades ------------------<>
+const edadMadre = 60;
+const pi = Math.PI;
+const esSoltero = false;
+let miEdad = 40;
