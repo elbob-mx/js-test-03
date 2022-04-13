1769bf97ad7488bc7ed5ed807d4da6a50e6b7e8b
elbob-mx <rob7th@gmail.com>
2022-04-13 14:49:05 -0500

    js-conditions-samples

--- aula4/Condiciones.js
+++ aula4/Condiciones.js
@@ -8,13 +8,16 @@ ciudadesDisponibles.push("Lima");
 ciudadesDisponibles.push("Santiago");
 console.log(ciudadesDisponibles);
 
-const edadCliente = 30;
+const edadCliente = 15;
 
 function cliente() {
   if (edadCliente >= 18) {
-    console.log(`El comprador es mayor de edad`);
+    console.log(`El comprador es mayor de edad.`);
+    console.log(`Ciudades disponibles para viajar: `);
+    console.log(ciudadesDisponibles);
   } else {
-  console.log(`El comprador NO es mayor de edad`);
+  console.log(`El comprador NO es mayor de edad.`);
+  console.log(`Elección no autorizada.`)
   };
 };
 
