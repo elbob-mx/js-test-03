63e400a6b1793e4955ffccc0bedb769f3a6a1b86
Roberto <rob7th@gmail.com>
2022-04-12 22:09:59 -0500

    js-samples-03

---
+++ 1-AsignacionVariables.js
@@ -0,0 +1,9 @@
+console.log("Asignando valores a las variables");
+
+let miNombre = "Roberto";
+const miApellido = " Gutiérrez";
+console.log(miNombre);
+
+miNombre = miNombre + miApellido;
+
+console.log(miNombre);
