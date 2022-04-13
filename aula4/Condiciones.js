1950158ff85a11bf7d2e83035adc85c4f2bc96d9
elbob-mx <rob7th@gmail.com>
2022-04-13 15:14:28 -0500

    js-conditionals

--- aula4/Condiciones.js
+++ aula4/Condiciones.js
@@ -8,7 +8,7 @@ ciudadesDisponibles.push("Lima");
 ciudadesDisponibles.push("Santiago");
 console.log(ciudadesDisponibles);
 
-const edadCliente = 15;
+const edadCliente = 18;
 
 function cliente() {
   if (edadCliente >= 18) {
@@ -17,8 +17,22 @@ function cliente() {
     console.log(ciudadesDisponibles);
   } else {
   console.log(`El comprador NO es mayor de edad.`);
-  console.log(`Elección no autorizada.`)
+  console.log(`Elección no autorizada.`);
+  console.log(`No es posible ofrecer viajes.`);
   };
 };
 
 cliente();
+
+console.log("mayor o igual que: ");
+console.log(edadCliente >= 18);
+console.log("mayor que: ");
+console.log(edadCliente > 18);
+console.log("menor o igual que: ");
+console.log(edadCliente <= 18);
+console.log("mayor que: ");
+console.log(edadCliente < 18);
+console.log("igual que: " /* un solo 'igual' sería darle un valor a 'edadCliente' */);
+console.log(edadCliente == 18);
+console.log("diferente de: ");
+console.log(edadCliente != 18);
